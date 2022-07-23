18a9d2598f24721cc2d043e4f7e8422b4ceddb4e
Andrei Bucur <abucur980@gmail.com>
2022-07-23 14:07:49 +0300

    add the logic

--- nButtons/src/app/app.component.ts
+++ nButtons/src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'nButtons';
+  inputVal = 0;
+  generateBtns = false;
+  TheLuckyButton = 0;
+  isLucky = -1;
+
+  checkBtnForClick() {
+    this.generateBtns = true;
+  }
+
+  getTheLuckyButton(num: number) {
+    this.TheLuckyButton = Math.floor(Math.random() * num);
+  }
+
+  checkIfLuckyBtn(index: number) {
+    if (index === this.TheLuckyButton) {
+      this.isLucky = index;
+    }
+  }
+
+  reloadPage() {
+    window.location.reload();
+ }
 }
